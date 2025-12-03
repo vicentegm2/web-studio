f4209d1fd86173964dfcd5a9f3cd9a9d02956f43
Vicente Gómez <vicente280228@gmail.com>
2025-12-03 22:55:49 +0000

    Try fixing this error: `Runtime Error: Error: Couldn't find next-intl co

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,16 @@
-import {NextIntlClientProvider, useMessages} from 'next-intl';
-
-export default function LocaleLayout({
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+ 
+export default async function LocaleLayout({
   children,
   params: {locale}
 }: {
   children: React.ReactNode;
   params: {locale: string};
 }) {
-  const messages = useMessages();
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
  
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,5 @@
 import { createLocalizedPathnamesNavigation } from 'next-intl/navigation';
-
-export const locales = ['en', 'es'] as const;
-export const localePrefix = 'always'; // Default
+import { locales, localePrefix } from './i18n';
 
 export const { Link, redirect, usePathname, useRouter } = createLocalizedPathnamesNavigation({
   locales,
